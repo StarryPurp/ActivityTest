ce66b398d0dcfb3a490a0e30a054d03d8042f267
StarryPurp <starbloom128@gmail.com>
2017-04-13 22:14:06 +0900

    #4 Complete?

--- app/src/main/java/bstar128/example/hansung/activitytest/MainActivity.java
+++ app/src/main/java/bstar128/example/hansung/activitytest/MainActivity.java
@@ -5,16 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edit1,edit2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Button butMain=(Button)findViewById(R.id.but_main);
         butMain.setOnClickListener(butMainHandler);
-
+        edit1=(EditText)findViewById(R.id.edit_main);
+        edit2=(EditText)findViewById(R.id.edit_tel);
     }
 
     View.OnClickListener butMainHandler=new View.OnClickListener() {
@@ -22,7 +24,12 @@ public class MainActivity extends AppCompatActivity {
         public void onClick(View v) {
             Intent intent=new Intent(MainActivity.this,SecondActivity.class);
                     //get-도 가능
+            String name=edit1.getText().toString();
+            String tel=edit2.getText().toString();
+            intent.putExtra("name",name);//첫번째 이름, 값
+            intent.putExtra("tel",tel);
             startActivity(intent);
+
         }
     };
 }
--- app/src/main/java/bstar128/example/hansung/activitytest/SecondActivity.java
+++ app/src/main/java/bstar128/example/hansung/activitytest/SecondActivity.java
@@ -1,18 +1,25 @@
 package bstar128.example.hansung.activitytest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
-
+    String name,tel;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
         Button butBack=(Button)findViewById(R.id.but_second);
         butBack.setOnClickListener(butBackHanlder);
+        Intent intent=getIntent();
+        name=intent.getStringExtra("name");
+        tel=intent.getStringExtra("tel");
+        TextView textResults=(TextView)findViewById(R.id.text_name);
+        textResults.setText("*성명: "+name+"\n"+"*전화번호: "+tel);
     }
     View.OnClickListener butBackHanlder=new View.OnClickListener() {
         @Override
